0582d8a97403c357a4189af03cc6ac5b60328a95
Omid Najaee <o.najaee@gmail.com>
2018-04-23 13:56:21 +0200

    add topic modeling results to the NERResult

--- src/routes/demo/modules/NERResult/index.js
+++ src/routes/demo/modules/NERResult/index.js
@@ -11,14 +11,16 @@ const Spinner = () => (
 const IndicatorsWrapperPure = ({ selectedIndex, data }) => {
   console.log('okay', data);
   return (
-    <Card style={{ marginTop: 10 }}>
-      <CardText>
-        <h3>Computer Vision</h3> 
-        <Indicator labels={['LOC', 'ORG', 'PER', 'DIS', 'PLC']} values={data[selectedIndex].cv_indicators} />
-        <h3>Text Classification</h3>
-        <Indicator labels={['LOC', 'ORG', 'PER', 'DIS', 'PLC']} values={data[selectedIndex].tc_indicators}  />
-      </CardText>
-    </Card>  
+      <Card style={{ marginTop: 10 }}>
+        <CardText>
+          <h3>Computer Vision</h3> 
+          <Indicator labels={['LOC', 'ORG', 'PER', 'DIS', 'PLC']} values={data[selectedIndex].cv_indicators} />
+          <h3>Text Classification</h3>
+          <Indicator labels={['LOC', 'ORG', 'PER', 'DIS']} values={data[selectedIndex].tc_indicators}  />
+          <h3>Text Classification (Topic Modeling)</h3>
+          <Indicator labels={['LOC', 'ORG', 'PER', 'DIS']} values={data[selectedIndex].tc_indicators_topic}  />
+        </CardText>
+      </Card>  
   );
 }
 
